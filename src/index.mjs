f7cb3489e7db42caba72a03dbd7d506dec010fcf
quan <quan_xiaoxiao@qq.com>
2024-05-06 15:47:26 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,3 @@
-import forwardRequest from './forwardRequest.mjs';
 import handleSocketRequest from './handleSocketRequest.mjs';
 
 export default handleSocketRequest;
-
-export {
-  handleSocketRequest,
-  forwardRequest,
-};
