8c666456dc8ee0dd62bb01e6051866b034c3878b
quan <quan_xiaoxiao@qq.com>
2024-07-23 09:13:31 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -2,13 +2,11 @@ import handleSocketRequest from './handleSocketRequest.mjs';
 import renderToHtml from './renderToHtml.mjs';
 import parseHtml from './parseHtml.mjs';
 import generateHtmlTag from './generateHtmlTag.mjs';
-import forwardHttpRequest from './forwardHttpRequest.mjs';
 import generateRouteMatchList from './generateRouteMatchList.mjs';
 import createHttpRequestHandler from './createHttpRequestHandler.mjs';
 
 export {
   generateRouteMatchList,
-  forwardHttpRequest,
   handleSocketRequest,
   createHttpRequestHandler,
   parseHtml,
