f4941f98411e38ba176b00c350721bc3e7cc0366
quan <quan_xiaoxiao@qq.com>
2024-07-24 16:38:31 +0800

    xxx

--- src/createHttpRequestHandler.mjs
+++ src/createHttpRequestHandler.mjs
@@ -44,8 +44,11 @@ export default (routeMatchList, logger) => ({
   },
   onHttpResponse: async (ctx) => {
     if (ctx.response) {
-      assert(ctx.response.promise && typeof ctx.response.promise.then === 'function');
-      await ctx.response.promise();
+      await new Promise((resolve) => {
+        ctx.response.promise = () => {
+          resolve();
+        };
+      });
     } else if (ctx.requestHandler.validate) {
       ctx.request.data = decodeContentToJSON(ctx.request.dataBuf, ctx.request.headers);
       if (!ctx.requestHandler.validate(ctx.request.data)) {
