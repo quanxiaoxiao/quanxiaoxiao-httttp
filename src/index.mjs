1a8285bafa0721f581e2bab6219c26a500c99614
quan <quan_xiaoxiao@qq.com>
2024-08-09 11:49:31 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,13 @@
 import handleSocketRequest from './handleSocketRequest.mjs';
 import generateRouteMatchList from './router/generateRouteMatchList.mjs';
 import createHttpRequestHandler from './router/createHttpRequestHandler.mjs';
-import forwardRequest from './forwardRequest.mjs';
 import * as constants from './constants.mjs';
-import decodeResponseStreamToJson from './decodeResponseStreamToJson.mjs';
+import readStream from './readStream.mjs';
 
 export {
   constants,
-  forwardRequest,
   generateRouteMatchList,
   handleSocketRequest,
   createHttpRequestHandler,
-  decodeResponseStreamToJson,
+  readStream,
 };
